a90b43d414610603d624eed5d9adad06e0b1a8a7
kitokoh <ibrahimkoubaye@outlook.com>
2024-10-01 11:53:07 +0300

    message

--- send_mail.py
+++ send_mail.py
@@ -1,30 +1,27 @@
+import os
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
-import os
 
-# Define email parameters
+# Retrieve the SMTP password from the environment variable
+password = os.getenv('SMTP_PASSWORD')
+
+# Other email parameters
 from_addr = "info@turknovatech.com"
 to_addr = "turk.novatech@gmail.com"
 subject = "Journal d'installation"
 body = "Veuillez trouver ci-joint le journal d'installation."
-password = "7LdXDMXLkQ7G"
 
-# Define the path to the log file
 log_file_path = os.path.expanduser("~/Desktop/journalInstallation.txt")
 
-# Create the email message
 msg = MIMEMultipart()
 msg['From'] = from_addr
 msg['To'] = to_addr
 msg['Subject'] = subject
-
-# Attach the body to the email
 msg.attach(MIMEText(body, 'plain'))
 
-# Attach the log file
 with open(log_file_path, "rb") as attachment:
     part = MIMEBase('application', 'octet-stream')
     part.set_payload(attachment.read())
@@ -32,17 +29,14 @@ with open(log_file_path, "rb") as attachment:
     part.add_header('Content-Disposition', f"attachment; filename={os.path.basename(log_file_path)}")
     msg.attach(part)
 
-# SMTP server configuration
 smtp_server = "mail.turknovatech.com"
 smtp_port = 587
 
-# Send the email
 try:
     server = smtplib.SMTP(smtp_server, smtp_port)
-    server.starttls()  # Upgrade to TLS
+    server.starttls()
     server.login(from_addr, password)
-    text = msg.as_string()
-    server.sendmail(from_addr, to_addr, text)
+    server.sendmail(from_addr, to_addr, msg.as_string())
     server.quit()
     print("[OK] - Email envoyé avec succès !")
 except Exception as e:
