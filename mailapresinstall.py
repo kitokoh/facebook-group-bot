4809ffb17f8e234a5a69beb3def83350a7399288
kitokoh <ibrahimkoubaye@outlook.com>
2024-10-06 10:51:03 +0300

    m

--- mailapresinstall.py
+++ mailapresinstall.py
@@ -1,26 +1,20 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import sys  # Pour récupérer les arguments de la ligne de commande
 
 # Fonction pour envoyer l'e-mail
 def send_email(client_email, sender_password, subject, message_body):
-    # Informations sur le compte e-mail expéditeur
     sender_email = "info@turknovatech.com"
-
-    # Configuration du serveur SMTP
     smtp_server = "mail.turknovatech.com"
     smtp_port = 587
 
-    # Configuration de l'e-mail
     msg = MIMEMultipart()
     msg['From'] = sender_email
     msg['To'] = client_email
     msg['Subject'] = subject
-
-    # Ajouter le corps de l'e-mail
     msg.attach(MIMEText(message_body, 'plain'))
 
-    # Envoyer l'e-mail via SMTP
     try:
         server = smtplib.SMTP(smtp_server, smtp_port)
         server.starttls()
@@ -37,34 +31,25 @@ def get_client_email():
         with open(r'C:\bon\info.txt', 'r') as file:
             for line in file:
                 if line.startswith("Email:"):
-                    # Extraire l'adresse e-mail après "Email: "
                     email = line.split("Email:")[1].strip()
                     return email
     except FileNotFoundError:
         print("Erreur : Le fichier info.txt n'a pas été trouvé.")
         return None
 
-# Fonction pour lire le mot de passe de l'expéditeur depuis licence.txt
-def get_sender_password():
-    try:
-        with open(r'C:\bon\licence.txt', 'r') as file:
-            # Lire la première ligne contenant le mot de passe
-            password = file.readline().strip()
-            return password
-    except FileNotFoundError:
-        print("Erreur : Le fichier licence.txt n'a pas été trouvé.")
-        return None
-
 # Exemple d'utilisation
 if __name__ == "__main__":
     client_email = get_client_email()
-    sender_password = get_sender_password()
+
+    # Récupérer le mot de passe transmis en argument depuis le script batch
+    if len(sys.argv) > 1:
+        sender_password = sys.argv[1]
+    else:
+        sender_password = None
 
     if client_email and sender_password:
         subject = "Votre sujet ici"
         message_body = "Bonjour, voici le contenu de votre e-mail."
-
-        # Appel de la fonction pour envoyer l'e-mail
         send_email(client_email, sender_password, subject, message_body)
     else:
         print("Aucune adresse e-mail ou mot de passe valide trouvée.")
